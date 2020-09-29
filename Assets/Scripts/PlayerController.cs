7bcce42d5d16d35cffe146593b309979ae592ef3
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-09-29 17:19:59 +0200

    Implemented Sprint base feature - Added sprintSpeed property 	modified:   Assets/Scripts/PlayerController.cs 	modified:   ProjectSettings/InputManager.asset

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
 
+    [SerializeField] float sprintSpeed = 30f;
+
     // Update is called once per frame
     void Update()
     {
         float xAxis = Input.GetAxis("Joystick X");
-
-        transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * speed;
+        if (Input.GetButton("Sprint"))
+        {
+            transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * sprintSpeed;
+        } else
+        {
+            transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * speed;
+        }        
     }
 }
