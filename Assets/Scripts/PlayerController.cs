a94f648d9d88d54a92755332b734d53f6a2d1ef1
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-09-29 17:23:35 +0200

    Refactored movements :
    - Added a method movePlayer
    	modified:   Assets/Scripts/PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
-
     [SerializeField] float sprintSpeed = 30f;
 
-    // Update is called once per frame
     void Update()
     {
         float xAxis = Input.GetAxis("Joystick X");
         if (Input.GetButton("Sprint"))
         {
-            transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * sprintSpeed;
+            movePlayer(xAxis, sprintSpeed);
         } else
         {
-            transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * speed;
+            movePlayer(xAxis, speed);
         }        
+    }
+
+    private void movePlayer(float xAxis, float desiredSpeed)
+    {
+        transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * desiredSpeed;
     }
 }
