76383dc2a0828f12abd4817739103e206df237fd
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-09-29 16:06:29 +0200

    Simple x movement on joystick input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Joystick X");
+
+        transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * speed;
+    }
+}
