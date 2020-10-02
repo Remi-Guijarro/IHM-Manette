480d4dc15db6ae58ae3f4d1dd9a84f5d9fb589f4
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-10-02 21:56:11 +0200

    Implement basic jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
+    [SerializeField] float impulse = 10f;
+    [SerializeField] float gravity = 9.81f;
+    [SerializeField] float fallMultiplier = 1.5f;
 
-    // Update is called once per frame
+    bool isGrounded = true;
+    float yVelocity;
+
+    // Cached variables
+    Rigidbody2D rigidbody;
+    BoxCollider2D collider;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
     void Update()
     {
         float xAxis = Input.GetAxis("Joystick X");
 
         transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * speed;
+
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, collider.size.y / 2);
+
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (isGrounded && Input.GetButton("Jump"))
+        {
+            yVelocity = impulse; 
+        }
+
+        else if (isGrounded)
+        {
+            yVelocity = 0;
+        }
+        else
+        {
+            yVelocity -= gravity * fallMultiplier * Time.deltaTime;
+            print(yVelocity);
+        }
+        transform.position += transform.up * yVelocity * Time.deltaTime;
     }
 }
