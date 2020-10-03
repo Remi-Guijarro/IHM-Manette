ff00d941df8815a931118a9b1132d36ef12d2c85
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-10-03 10:45:19 +0200

    changed movePlayer method name to move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,14 +12,14 @@ public class PlayerController : MonoBehaviour
         float xAxis = Input.GetAxis("Joystick X");
         if (Input.GetButton("Sprint"))
         {
-            movePlayer(xAxis, sprintSpeed);
+            move(xAxis, sprintSpeed);
         } else
         {
-            movePlayer(xAxis, speed);
+            move(xAxis, speed);
         }        
     }
 
-    private void movePlayer(float xAxis, float desiredSpeed)
+    private void move(float xAxis, float desiredSpeed)
     {
         transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * desiredSpeed;
     }
