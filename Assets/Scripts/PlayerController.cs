d3b4d32debeca7ac06c3dc586261511179778463
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-10-03 11:15:59 +0200

    Implemented dash feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
+    [SerializeField] float dashDuration = 1f;
+    [SerializeField] float dashSpeed = 20f;
+    [SerializeField] float dashDistance = 0.01f;
+    [SerializeField] float dashIncrement = 0.5f;
+    private float currentDashTime;
+
 
     // Update is called once per frame
     void Update()
     {
         float xAxis = Input.GetAxis("Joystick X");
-
         transform.position += new Vector3(xAxis, 0f) * Time.deltaTime * speed;
+        Dash(xAxis);
+    }
+
+    private void MoveTo(Vector3 location, float speed)
+    {
+        transform.position += location * speed * Time.deltaTime;
+    }
+
+    private void Dash(float xAxis)
+    {
+        if (Input.GetButton("Dash"))
+        {
+            Vector3 moveDirection;
+            if (currentDashTime < dashDuration)
+            {
+                moveDirection = new Vector3(xAxis * dashDistance, 0.0f);
+                currentDashTime += dashIncrement;
+            }
+            else
+            {
+                moveDirection = Vector3.zero;
+            }
+            MoveTo(moveDirection, dashSpeed);
+        }
+        else
+        {
+            currentDashTime = 0f;
+        }
     }
 }
+ 
