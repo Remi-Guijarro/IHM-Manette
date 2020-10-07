96cf9a321cdb271f50d804b4d0279772a402c03c
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-10-07 18:54:39 +0200

    Remove unused 'using's

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Linq.Expressions;
-using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
