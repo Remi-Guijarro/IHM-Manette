9b6cb065c3e696732610d03b1c4dac838b5d5acc
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-10-13 18:04:45 +0200

    Remove unused functions

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -251,16 +251,6 @@ public class PlayerController2D : MonoBehaviour
         this.velocity.y *= 1f - this.wallDrag;
     }
 
-    private bool DoWallJump(ColliderDistance2D colliderDistance)
-    {
-        return IsCollidingWithHorizontalSurface(colliderDistance) && inputManager.JumpPressed();
-    }
-
-    private bool IsCollidingWithHorizontalSurface(ColliderDistance2D colliderDistance)
-    {
-        return Vector2.Angle(colliderDistance.normal, Vector2.up) == 90f;
-    }
-
     private void ComputeWallJumpVelocity()
     {
         this.velocity.x = -1 * this.velocity.x * this.wallJumpBoost;
